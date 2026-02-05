6cc16e8bbc4ee44ef50e70b147ae49d8bda6f63c
Sabin Ciurusniuc <saciurus@f2r7s13.clusters.42paris.fr>
2026-02-05 18:44:03 +0100

    s

--- cpp00/ex00.cpp
+++ cpp00/ex00.cpp
@@ -1,19 +1,22 @@
+#include <cctype>
 #include <iostream>
 #include <string>
-#include <cctype>
-using namespace std;
 
-int     main(int ac , char **av)
+using namespace	std;
+
+int	main(int ac, char **av)
 {
+	unsigned long		i;
+	int		j;
+	string	argument;
+	string	line;
+
 	if (ac == 1)
 	{
 		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << endl;
-		return 0;
+		return (0);
 	}
-        int     i;
-	int	j = 1;
-        string  argument;
-	string	line;
+	j = 1;
 	while (j < ac)
 	{
 		argument = av[j];
@@ -29,5 +32,5 @@ int     main(int ac , char **av)
 	}
 	if (ac > 1)
 		cout << line << endl;
-	return 0;
+	return (0);
 }
